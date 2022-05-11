334da680325edb770505a04ac5c2356f7b5c0f4a
Isabel Cheung <isabelczyi@gmail.com>
2022-05-11 17:16:35 +1000

    imported to nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,19 +3,23 @@ import { Link } from "react-router-dom";
 // import Burger from "./Burger";
 import RightNav from './RightNav';
 import LoginControlNav from "./LoginControlNav";
+import AfterSignedInNav from "./AfterSignedInNav";
 import "./Nav.css";
 
 const Nav = () => {
   return (
       <div className="navbar">
+      <div style={{display: "flex"}}>
         <Link to="/">
           <img
             src="https://res.cloudinary.com/gohkt/image/upload/v1652185178/go1-hackathon/go1logo_lgqrcr.png"
-            alt="go1-logo"
+            alt="go1-logo" style={{width:"50px"}}
           ></img>
         </Link>
       <RightNav />
-      <LoginControlNav />
+      </div>
+      <AfterSignedInNav />
+      {/* <LoginControlNav /> */}
     </div>
   );
 };
