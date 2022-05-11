697c17eb73d1a1b469c39757f02b4390f4e9da29
Isabel Cheung <isabelczyi@gmail.com>
2022-05-11 17:15:41 +1000

    added icons to left side of nav

--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const RightNav = () => {
   return (
       <div className="nav-middle">
-          <Link to="/">Home</Link>
-          <Link to="/events">Explore</Link>
-          <Link to="/hosts">Groups</Link>
-          <Link to="/heroes">Mentors</Link>
+          <Link to="/"><i class="fa-solid fa-house-chimney"></i>  Home</Link>
+          <Link to="/events"><i class="fa-solid fa-compass"></i>  Explore</Link>
+          <Link to="/hosts"><i class="fa-solid fa-user-group"></i>  Groups</Link>
+          <Link to="/heroes"><i class="fa-solid fa-people-arrows-left-right"></i>  Experts</Link>
       </div>
 
   );
