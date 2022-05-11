8673ef1345edef4397a1c229e966119f1a78e053
Isabel Cheung <isabelczyi@gmail.com>
2022-05-11 15:51:35 +1000

    OMG filter in banner done

--- src/pages/HeroesPage.jsx
+++ src/pages/HeroesPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import HeroCard from "../components/UserCard/HeroCard";
-import "./EventsPage.css";
+import "./HomePage.css";
+ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+ import { fasearch } from '@fortawesome/free-solid-svg-icons'
 
 const HeroesPage = () => {
   const [heroList, setHeroList] = useState([]);
@@ -21,13 +23,39 @@ const HeroesPage = () => {
   }, []);
 
   return (
-    <div className="page-wrapper">
-      <div className="cover_events">
+    <div className="home-page">
+      <div className="cover">
         <img
           src="https://res.cloudinary.com/gohkt/image/upload/v1652184996/go1-hackathon/go1banner_qx7ong.jpg"
           alt="go1-banner"
         ></img>
-        <div class="cover-text"></div>
+        <div class="cover-text">
+          <div class="form-selections">
+            Expertise
+            <input type="text" name="name" placeholder="What skill are you after?"/>
+          </div>
+          <div class="form-selections" style={{paddingRight: "64px"}}>
+            Services <br/>
+            <select>
+                <option value="" disabled selected>What service do you need?</option>
+                <option value="Mentoring">Mentoring</option>
+                <option value="Coaching">Coaching</option>
+                <option value="Networking">Networking</option>
+                <option value="Speaking">Speaking</option>
+                <option value="Teaching">Teaching</option>
+            </select>
+          </div>
+          <div class="form-selections" style={{paddingRight: "64px"}}>
+            Community <br/>
+            <select value="My organisation">
+                <option value="My organisation">My organisation</option>
+                <option value="Go1 Community">Go1 Community</option>
+            </select>
+          </div>
+          <div class="magnifying-icon">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
       </div>
 
       <div className="page-main">
