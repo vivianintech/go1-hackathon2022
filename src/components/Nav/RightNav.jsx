f870a33f7a38af2371a5b8bfa01ae403e3769b27
Isabel Cheung <isabelczyi@gmail.com>
2022-05-11 16:43:53 +1000

    removed unnecessary lines

--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -1,51 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 
-
-const Ul = styled.ul`
-  list-style: none;F
-  display: flex;
-  flex-flow: row nowrap;
-
-  li {
-    padding: 18px 10px;
-  }
-
-  @media (max-width: 768px) {
-    flex-flow: column nowrap;
-    background-color: #05386b;
-    position: fixed;
-    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
-    top: 0;
-    right: 0;
-    height: 100vh;
-    width: 300px;
-    padding-top: 3.5rem;
-    transition: transform 0.3s ease-in-out;
-    z-index: 10;
-
-    a {
-      text-decoration: none;
-      color: white;
-      font-weight: bold;
-      font-size: larger;
-    }
-
-    li {
-      color: #ffff;
-    }
-  }
-`;
-
-const RightNav = ({ open }) => {
+const RightNav = () => {
   return (
       <div className="nav-middle">
           <Link to="/">Home</Link>
-          <Link to="/events">Events</Link>
+          <Link to="/events">Explore</Link>
+          <Link to="/hosts">Groups</Link>
           <Link to="/heroes">Mentors</Link>
-          <Link to="/hosts">Hosts</Link>
-          <Link to="/contact">Contact Us</Link>
       </div>
 
   );
