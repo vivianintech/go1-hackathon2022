cf31f34189e360a28d65c800f4b39e5f2b426b26
Isabel Cheung <isabelczyi@gmail.com>
2022-05-12 21:17:20 +1000

    email-fix

--- src/pages/ProfileUpdated.jsx
+++ src/pages/ProfileUpdated.jsx
@@ -3,7 +3,7 @@ import "./Style/ExplorePage.css";
 
 const ExplorePage = () => {
   return (
-    <a href="/emailmatch">
+    <a href="/learnertomentor">
       <img
         src="https://res.cloudinary.com/dyr0u2nqe/image/upload/v1652353566/Learner_2_vnvjnn.svg"
         alt="results page"
