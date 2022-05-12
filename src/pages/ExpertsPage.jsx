db93204f53548e1228990b29f00716e2584592c8
Vivian Nguyen <vivian.nguyen@go1.com>
2022-05-12 19:12:56 +1000

    fix issue with deployment

--- src/pages/ExpertsPage.jsx
+++ src/pages/ExpertsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {ButtonFilled} from "@go1d/go1d";
+import { ButtonFilled } from "@go1d/go1d";
 import ExpertCard from "../components/UserCard/ExpertCard";
 import "./Style/HomePage.css";
 import { EXPERTS, SERVICES } from "../Data/Constantsv2";
@@ -28,21 +28,41 @@ const ExpertsPage = () => {
   return (
     <>
       <div className="home-page">
-        <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '32px', marginBottom: '32px'}}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "32px",
+            marginBottom: "32px",
+          }}
+        >
           <div className="header-landing-page">Join our coaching circle</div>
-          <ButtonFilled color="accent" width="auto" icon={IconUser}><div style={{fontFamily: 'Manrope'}}>Update profile to join</div></ButtonFilled>
+          <ButtonFilled color="accent" width="auto" icon={IconUser}>
+            <div style={{ fontFamily: "Manrope" }}>Update profile to join</div>
+          </ButtonFilled>
         </div>
         <div className="cover">
-
           <img
             src="https://res.cloudinary.com/dyr0u2nqe/image/upload/v1652319506/Banner_ok5dvq.png"
-            alt="go1-banner" style={{objectFit: 'contain'}}
+            alt="go1-banner"
+            style={{ objectFit: "contain" }}
           ></img>
           <div className="above-img">
-             <div style={{color: '#114954', fontWeight: 'bold', fontSize: '24px', paddingBottom: '16px'}}>Find an expert</div>
-             <div style={{paddingBottom: '32px'}}>Connect with a mentor or coach to achieve your potential</div>
+            <div
+              style={{
+                color: "#114954",
+                fontWeight: "bold",
+                fontSize: "24px",
+                paddingBottom: "16px",
+              }}
+            >
+              Find an expert
+            </div>
+            <div style={{ paddingBottom: "32px" }}>
+              Connect with a mentor or coach to achieve your potential
+            </div>
             <div class="cover-form">
-              <div class="form-selections" style={{paddingLeft: '8px'}}>
+              <div class="form-selections" style={{ paddingLeft: "8px" }}>
                 Expertise
                 <input
                   type="text"
@@ -79,36 +99,35 @@ const ExpertsPage = () => {
           </div>
         </div>
 
-
-      <div className="expert-wrapper-container">
-        <div className="expert-wrapper">
-          <div className="expert-wrapper-feature">Featured Experts</div>
-          <div className="expert-flex">
-            {EXPERTS.slice(0,4).map((data, key) => {
-              return <ExpertCard key={key} heroData={data} />;
-            })}
+        <div className="expert-wrapper-container">
+          <div className="expert-wrapper">
+            <div className="expert-wrapper-feature">Featured Experts</div>
+            <div className="expert-flex">
+              {EXPERTS.slice(0, 4).map((data, key) => {
+                return <ExpertCard key={key} heroData={data} />;
+              })}
+            </div>
           </div>
-        </div>
 
-        <div className="expert-wrapper">
-          <div className="expert-wrapper-feature">Services</div>
-          <div className="expert-service">
-            {SERVICES.map((data, key) => {
-              return <ServiceCard key={key} services={data} />;
-            })}
+          <div className="expert-wrapper">
+            <div className="expert-wrapper-feature">Services</div>
+            <div className="expert-service">
+              {SERVICES.map((data, key) => {
+                return <ServiceCard key={key} services={data} />;
+              })}
+            </div>
           </div>
-        </div>
 
-        <div className="expert-wrapper" style={{marginBottom: '32px'}}>
-          <div className="expert-wrapper-feature">Recently Viewed</div>
-          <div className="expert-flex">
-            {EXPERTS.slice(4).map((data, key) => {
-              return <ExpertCard key={key} heroData={data} />;
-            })}
+          <div className="expert-wrapper" style={{ marginBottom: "32px" }}>
+            <div className="expert-wrapper-feature">Recently Viewed </div>
+            <div className="expert-flex">
+              {EXPERTS.slice(4).map((data, key) => {
+                return <ExpertCard key={key} heroData={data} />;
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
